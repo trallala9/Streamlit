6107e91e025f84639a37d149007d82cb0f68450f
trallala9 <trallala9@o2.pl>
2022-05-13 20:12:49 +0200

    Add classifier names function, parameters,slider

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.write("""
 # Explore a classfier!
 """)
 
-dataset_name = st.sidebar.selectbox("Select dataset",("Iris", "Cancer", "Wine dataset"))
+dataset_name = st.sidebar.selectbox("Select dataset",("Iris", "House prices", "Wine dataset"))
 st.write(dataset_name)
 
 classifier_name =  st.sidebar.selectbox("Select dataset",("KNN", "SVN", "Random Forest"))
@@ -17,8 +17,8 @@ classifier_name =  st.sidebar.selectbox("Select dataset",("KNN", "SVN", "Random 
 def get_dataset(dataset_name):
     if dataset_name == "Iris":
         data = datasets.load_iris()
-    elif dataset_name == "Cancer":
-        data = datasets.load_cancer() 
+    elif dataset_name == "Prices":
+        data = datasets.load_prices() 
     else:
         data = datasets.load_wine() 
     X = data.data
@@ -26,4 +26,14 @@ def get_dataset(dataset_name):
     return X, y   
 
 X, y = get_dataset(dataset_name)
-st.write("Shape of dataset", X.shape)          
+st.write("Shape of dataset",X.shape)
+st.write("Number of classes", len(np.unique(y)))
+
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name == "KNN":
+        K = st.sidebar.slider("K", 1, 15) 
+        params["K"] = K
+    return params 
+
+add_parameter_ui(classifier_name)
