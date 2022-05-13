10423d8ca57126dcca78f47d54eb2072bc56b7ba
trallala9 <trallala9@o2.pl>
2022-05-13 19:25:44 +0200

    load and returned dataset with function, add and displayX.shape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import sklearn as sklearn
+from sklearn import datasets
+import numpy as np
 
 st.title("hello")
 
@@ -9,3 +11,19 @@ st.write("""
 
 dataset_name = st.sidebar.selectbox("Select dataset",("Iris", "Cancer", "Wine dataset"))
 st.write(dataset_name)
+
+classifier_name =  st.sidebar.selectbox("Select dataset",("KNN", "SVN", "Random Forest"))
+
+def get_dataset(dataset_name):
+    if dataset_name == "Iris":
+        data = datasets.load_iris()
+    elif dataset_name == "Cancer":
+        data = datasets.load_cancer() 
+    else:
+        data = datasets.load_wine() 
+    X = data.data
+    y = data.target
+    return X, y   
+
+X, y = get_dataset(dataset_name)
+st.write("Shape of dataset", X.shape)          
