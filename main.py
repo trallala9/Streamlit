72b75da4102bb764071ab4227f0272d270a97e85
trallala9 <trallala9@o2.pl>
2022-05-15 19:37:48 +0200

    Add get_classifier func. based on dataset name and parms ,imported  classifiers, removed doubled sliders

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
-import sklearn as sklearn
 from sklearn import datasets
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+
 import numpy as np
 
 st.title("hello")
@@ -12,12 +17,12 @@ st.write("""
 dataset_name = st.sidebar.selectbox("Select dataset",("Iris", "House prices", "Wine dataset"))
 st.write(dataset_name)
 
-classifier_name =  st.sidebar.selectbox("Select dataset",("KNN", "SVN", "Random Forest"))
+classifier_name =  st.sidebar.selectbox("Select classifier",("KNN", "SVM", "Random Forest"))
 
 def get_dataset(dataset_name):
     if dataset_name == "Iris":
         data = datasets.load_iris()
-    elif dataset_name == "Prices":
+    elif dataset_name == "House Prices":
         data = datasets.load_prices() 
     else:
         data = datasets.load_wine() 
@@ -34,8 +39,9 @@ def add_parameter_ui(clf_name):
     if clf_name == "KNN":
         K = st.sidebar.slider("K", 1, 15) 
         params["K"] = K
-    elif clf_name == "SVN":
-        C = st.sidebar.slider("C", 0.01, 10.0)   
+    elif clf_name == "SVM":
+        C = st.sidebar.slider("C", 0.01, 10.0)  
+        params["C"] = C 
     else:
         max_depth = st.sidebar.slider("max_depth", 2, 15)
         n_estimators = st.sidebar.slider("n_estimators", 1, 100) 
@@ -43,4 +49,21 @@ def add_parameter_ui(clf_name):
         params["n_estimators"] = n_estimators   
     return params 
 
-add_parameter_ui(classifier_name)
+params = add_parameter_ui(classifier_name)
+
+def get_classifier(clf_name, params):
+    
+    if clf_name == "KNN":
+        clf = KNeighborsClassifier(n_neighbors=params["K"])
+    elif clf_name == "SVM":
+        clf = SVC(C=params["C"]) 
+    else:
+        clf = RandomForestClassifier(n_estimators=params["n_estimators"],
+                                max_depth=params["max_depth"], random_state=1234)
+    return clf
+
+clf = get_classifier(classifier_name, params)
+
+
+
+   
